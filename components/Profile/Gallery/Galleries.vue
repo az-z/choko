0d89eefefe4a8bc48e0159e61c02486827d12ba2
Your Name <you@example.com>
2020-09-27 23:06:31 +0300

    exchange metod of the folders

--- components/Profile/Gallery/Galleries.vue
+++ components/Profile/Gallery/Galleries.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
 .galleries
-  .gallery(
-    :is="currentComponent"
-    v-for="(gallery, index) in galleries" :key="index"
-    :gallery="gallery"
-  )
+  .exchange_bar
+    svg-icon(name="b" @click="changeComponent('OneGallery')").exchange__grid 
+    svg-icon(name="bfc" @click="changeComponent('Folder')").exchange__folder
+  .galleries__list
+    .gallery(
+      :is="currentComponent"
+      v-for="(gallery, index) in galleries" :key="index"
+      :gallery="gallery"
+    )
 </template>
 <script>
 import { mapGetters, mapActions } from 'vuex'
@@ -26,7 +30,10 @@ export default {
   methods: {
     ...mapActions({
       updateGalleries: 'Galleries/updateGalleries'
-    })
+    }),
+    changeComponent: function (component) {
+      this.currentComponent = component
+    }
   },
   mounted: function () {
     this.updateGalleries()
@@ -35,9 +42,40 @@ export default {
 </script>
 <style lang="scss" scoped>
 .galleries {
-  margin-top: map-get($indents, veryBig);
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-gap: 20px;
+  &__list {
+    margin-top: map-get($indents, veryBig);
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-gap: 20px;
+  }
 }
+
+.exchange_bar {
+  margin-top: 1rem;
+  background-color:#9ed868;
+  border-radius: 1rem;
+  display: flex;
+  justify-content: right;
+  padding: .5rem;
+  align-items: center;
+  
+  
+  .exchange__grid  {
+    cursor: pointer;
+    height: 27px;
+    width: 27px;
+    display: block;
+    margin-left: auto;
+ 
+
+  }
+  .exchange__folder {
+    cursor: pointer;
+    height: 32px;
+    width: 32px;
+    display: block;
+    margin-left: 12px;
+  }
+}
+
 </style>
